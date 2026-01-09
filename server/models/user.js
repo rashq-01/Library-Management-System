5cebeeb35d60ed375ff6281829771e4c0892795f
rashq <rashq122@gmail.com>
2026-01-09 21:10:26 +0530

    Completing userModel

---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        fullName : {
+            type : String,
+            required : true,
+            trim : true,
+
+        },
+        rollNumber : {
+            type : String,
+            required : true,
+            unique : true,
+            trim : true
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true,
+            lowercase : true,
+            trim : true,
+        },
+        password : {
+            type : String,
+            required : true
+        },
+        role : {
+            type : String,
+            enum  : ["admin","student"],
+            default : "student"
+        }
+    },
+    {
+        timestamps : true
+    }
+);
